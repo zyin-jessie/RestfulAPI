0aa609f3bd0c709046c3ce94a97d8523a8bb241a
Jessie Bartolome <macjessie.bartolome@gmail.com>
2024-10-06 01:12:42 +0800

    Update get.js

--- get.js
+++ get.js
@@ -1,11 +1,7 @@
 app.get("/users", (request, response) => {
-    const query = "SELECT id, fname, lname FROM registered";
+    const query = "SELECT * FROM registered";
 
-    db.query(query, (err, results) => {
-        if (err) {
-            console.error('Database query failed: ', err);
-            return response.status(500).json({ error: 'Internal Server Error' });
-        }
-        return response.json(results); 
+    db.query(query, (err, data) => {
+        return response.json(data); 
     });
 });
