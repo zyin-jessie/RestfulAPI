00bec314bfebae77deae745902d80a45aa1fbc5e
Jessie Bartolome <macjessie.bartolome@gmail.com>
2024-10-06 01:16:00 +0800

    Update get.js

--- get.js
+++ get.js
@@ -1,7 +1,13 @@
+//Mysql
 app.get("/users", (request, response) => {
     const query = "SELECT * FROM registered";
 
     db.query(query, (err, data) => {
         return response.json(data); 
     });
+});
+
+//Local Data
+app.get("/users", (request, response) => {
+    return response.send(mockUsers); 
 });
